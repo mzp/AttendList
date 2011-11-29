48177690858e73c25586993724701586f8d4bd7d
mzp <mzpppp@gmail.com>
2011-11-30 07:59:04 +0900

    twitterへの認証をつけた

--- src/main/scala/MainPlan.scala
+++ src/main/scala/MainPlan.scala
@@ -18,13 +18,19 @@ class MainPlan extends unfiltered.filter.Plan {
     engine
   }
 
-  def twitter = {
-    val CONSUMER = Consumer("6bBnS4NHAfDp2g8EYYekfA", "q8JvPVxKD1mjddya1a9lZQ28lIsIL7NwbXEbxrnd8")
-    val http = new Http
-    val redirectUrl = "http://example.com/service/callback"
-    val reqToken = http(Auth.request_token(CONSUMER, redirectUrl))
-    val url = Auth.authorize_url(reqToken).to_uri
-    Redirect(url.toString())
+  def twitter[A](request : HttpRequest[A], url : String) = {
+    request match {
+      case RemoteAddr(host) =>
+        val CONSUMER = Consumer("6bBnS4NHAfDp2g8EYYekfA", "q8JvPVxKD1mjddya1a9lZQ28lIsIL7NwbXEbxrnd8")
+        val http = new Http
+        val scheme = if(request.isSecure) "https" else "http"
+        val redirectUrl : String= scheme + "://" + request.headers("host").next + "/callback/" + url
+        println(redirectUrl)
+        val reqToken = http(Auth.request_token(CONSUMER, redirectUrl))
+        Redirect(Auth.authorize_url(reqToken).to_uri.toString)
+      case _ =>
+        BadRequest
+    }
   }
 
   def intent = { case request =>
@@ -33,7 +39,7 @@ class MainPlan extends unfiltered.filter.Plan {
         Pass
       case Path("/") =>
         Ok ~> Scalate(request, "index.scaml", "title" -> "PartakeList")
-      case Path("/create") =>
-        twitter
+      case Path("/create") & Params(params) =>
+        twitter(request, params("url").firstOption.getOrElse(""))
     }}}
 
