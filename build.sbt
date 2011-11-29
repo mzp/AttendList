17daa2ba84ed5123208988fd95644eaba27a9972
mzp <mzpppp@gmail.com>
2011-11-30 00:05:20 +0900

    distach.twitter追加

--- build.sbt
+++ build.sbt
@@ -6,4 +6,10 @@ libraryDependencies += "net.databinder" %% "unfiltered-filter" % "0.5.1"
 
 libraryDependencies += "net.databinder" %% "unfiltered-scalate" % "0.5.1"
 
+libraryDependencies += "net.databinder" %% "dispatch-core" % "0.8.6"
+
+libraryDependencies += "net.databinder" %% "dispatch-oauth" % "0.8.6"
+
+libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.6"
+
 libraryDependencies +=  "javax.servlet" % "servlet-api" % "2.5"
--- src/main/scala/MainPlan.scala
+++ src/main/scala/MainPlan.scala
@@ -6,6 +6,10 @@ import org.fusesource.scalate.TemplateEngine
 import org.fusesource.scalate.layout.DefaultLayoutStrategy
 import java.io.{File => JFile}
 
+import dispatch._
+import dispatch.oauth._
+import dispatch.twitter._
+
 class MainPlan extends unfiltered.filter.Plan {
   implicit lazy val engine = {
     val path = config.getServletContext.getRealPath("/")
@@ -14,9 +18,22 @@ class MainPlan extends unfiltered.filter.Plan {
     engine
   }
 
-  def intent = {
-    case req@Path(_) =>
-      println( )
-      Ok ~> Scalate(req, "index.scaml")(engine)
+  def twitter = {
+    val CONSUMER = Consumer("6bBnS4NHAfDp2g8EYYekfA", "q8JvPVxKD1mjddya1a9lZQ28lIsIL7NwbXEbxrnd8")
+    val http = new Http
+    val redirectUrl = "http://example.com/service/callback"
+    val reqToken = http(Auth.request_token(CONSUMER, redirectUrl))
+    val url = Auth.authorize_url(reqToken).to_uri
+    Redirect(url.toString())
   }
-}
+
+  def intent = { case request =>
+    request match {
+      case Path(Seg("static" :: _)) | Path("/favicon.ico") =>
+        Pass
+      case Path("/") =>
+        Ok ~> Scalate(request, "index.scaml", "title" -> "PartakeList")
+      case Path("/create") =>
+        twitter
+    }}}
+
